97470f5453db69b99c203c1d8ed3f2a4b9907048
AlyssaKrikland <54543481+AlyssaKrikland@users.noreply.github.com>
2026-04-26 14:39:46 +0800

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -94,6 +94,8 @@ def prefun():
     for i in model:
         r_p.append(float(i.predict_proba(pred_data)[0][1]))
     proba = round(sum(r_p)/len(r_p)*100, 3)
+
+    st.write(pred_data)
     
     with st.expander("**Predict result**", True):
         # st.html(f"<div style='text-align: center; font-weight: bold; color:#4676C8;'>Model optimal threshold {threshold},{sum(r_p)/len(r_p)}</div><hr>")
